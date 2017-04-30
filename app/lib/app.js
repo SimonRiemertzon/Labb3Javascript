c67226085daefd9be3171e3d3a8b8e139c695dcf
Siniom <simon.riemertzon@gmail.com>
2017-05-01 01:49:23 +0200

    Ssn function is working as intended. Next up, complete the form.

--- app/lib/app.js
+++ app/lib/app.js
@@ -1,4 +1,4 @@
-var labbApp = angular.module('labbApp', ['ngRoute']);
+var labbApp = angular.module('labbApp', ['ngRoute', 'ngMessages']);
 
 labbApp.config(function($routeProvider){
     $routeProvider.when('/partial' , {
@@ -37,22 +37,71 @@ labbApp.controller('partialTwoController', ['$scope', function ($scope) {
         ssn : ""
 
     };
+
  */
 
+    function isInputCorrect(newValue) {
+        console.log("Nu körs isInputCorrect " + newValue );
+        if (/\d{8}-\d{4}$/.test(newValue)) {
+            console.log(newValue);
+            if (isSsnValid(newValue)) {
+                $scope.showMyErrorMessage = false;
 
+            } else {
+                $scope.errorMsg = "Your control number isn't valid";
+                $scope.showMyErrorMessage = true;
 
-    $scope.$watch(ssnModel,
-        function(newValue, oldValue) {
-        if(newValue) {
-            console.log("Hello " + newValue);
+            }
+        } else {
+            $scope.errorMsg = "Not a valid ssn";
+            $scope.showMyErrorMessage = true;
+
         }
 
-    });
+    };
 
 
+    $scope.$watch('userSsn', isInputCorrect);
 
+/*
+****1****
+    labbApp.directive('userSsn', function() {
+    return {
+        restrict: 'A',
+
+        require: 'ngModel',
+
+        link: function (scope, element, attr, ctrl) {
+            function customValidator(ngModelValue) {
+                if (/\d{8}-\d{4}$/.test(ngModelValue)) {
+                    console.log(ngModelValue);
+                    ctrl.$setValidity('regexValidator', true);
+
+                    if (isSsnValid(ngModelValue)) {
+                        ctrl.$setValidity('securityNumberValidator', true);
+
+                    } else {
+                        $scope.errorMsg = "Kontrollsiffran är fel";
+                        ctrl.$setValidity('securityNumberValidator', false);
+                    }
+                } else {
+                    $scope.errorMsg = "Du angav ej ett korrekt personnummer. Försök igen!";
+                    ctrl.$setValidity('regexValidator', false);
+
+
+                }
+
+                return ngModelValue;
+            }
+            ctrl.$parsers.push(customValidator);
+        }
+    };
+});
+
+/*
     $scope.isInputCorrect = function isInputCorrect(ssn) {
         if (/\d{8}-\d{4}$/.test(ssn)) {
+            console.log(ssn);
             if (isSsnValid(ssn)) {
 
             } else {
@@ -68,12 +117,19 @@ labbApp.controller('partialTwoController', ['$scope', function ($scope) {
     };
 
 
+*/
+
+
+
+
     function isSsnValid(ssn) {
         var ssnOnlyNumbers = ssn.replace('-', '');
         var sum = 0;
 
+
         for (var i = 2; i < ssnOnlyNumbers.length - 1; i++) {
             if (i % 2 === 0) {
+
                 var currentSum = ssnOnlyNumbers.charAt(i) * 2;
                 sum += currentSum;
                 if (currentSum > 9) {
@@ -83,13 +139,13 @@ labbApp.controller('partialTwoController', ['$scope', function ($scope) {
                 sum += Number(ssnOnlyNumbers.charAt(i));
             }
         }
+
         var securityNumber = (10 - (sum % 10)) % 10;
-        console.log(securityNumber);
         return (securityNumber == ssn.charAt(12));
-    }
+    };
 
 
-    function calculateAge(ssn) {
+    $scope.calculateAge = function calculateAge(ssn) {
         sliceUpSsn(ssn, true);
         yourAge = currentYear - userYear;
 
@@ -100,10 +156,10 @@ labbApp.controller('partialTwoController', ['$scope', function ($scope) {
             yourAge--;
         }
         return yourAge;
-    }
+    };
 
 
-    function sliceUpSsn(ssn) {
+    $scope.sliceUpSsn = function sliceUpSsn(ssn) {
 
         userYear = Number(ssn.slice(0, 4));
         userMonth = Number(ssn.slice(4, 6));
